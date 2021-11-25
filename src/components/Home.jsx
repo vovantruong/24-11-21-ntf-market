dd5e11fb2488d53db87e5066ccb3abe27b3a30e5
Võ Văn Trường <56033678+vovantruong@users.noreply.github.com>
2021-11-25 15:31:43 +0700

    commit add components

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,14 @@
 import React from "react"
 import Footer from "./Header_Footer/Footer";
 import Header from "./Header_Footer/Header"
+import CreatorNetwork from './Home/CreatorNetwork'
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function Home() {
   return(
     <div id="home">
       <Header />
-      <h1>Home</h1>
+     <CreatorNetwork />
       <Footer />
     </div>
   )
---
+++ src/components/Home/CreatorNetwork.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function CreatorNetwork() {
+    return (
+        <section id="creator__network">
+        </section>
+    )
+}
+
+export default CreatorNetwork
