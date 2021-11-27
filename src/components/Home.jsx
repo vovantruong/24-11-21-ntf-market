63721ff694bc35308845f3258502e041a5b8c3f8
Võ Văn Trường <56033678+vovantruong@users.noreply.github.com>
2021-11-27 22:56:59 +0700

    commit complete components crypter in components home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import Footer from "./Header_Footer/Footer";
 import Header from "./Header_Footer/Header";
 import CreatorNetwork from "./Home/CreatorNetwork";
+import Seller from "./Home/Seller";
+import Crypter from "./Home/Crypter";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import '../assets/css/Home.css'
-import Seller from "./Home/Seller";
 
 function Home() {
   return (
@@ -12,6 +14,7 @@ function Home() {
       <Header />
       <CreatorNetwork />
       <Seller/>
+      <Crypter />
       <Footer />
     </div>
   );
