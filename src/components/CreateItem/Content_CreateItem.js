ac63af72a2319d09792291cef7210f0e11713d36
hieu294 <tranthehieu12>
2021-11-30 14:39:25 +0700

    update CreateItem 90%

--- src/components/CreateItem/Content_CreateItem.js
+++ src/components/CreateItem/Content_CreateItem.js
@@ -24,10 +24,10 @@ const CreateItem = () => {
             <Col lg={7}>
               <div className="left__CreateSimple">
                 <Row>
-                  <Col className=" Collectible" lg={7} md={6} >
+                  <Col className=" Collectible" lg={8} md={6} >
                     <h1>Create single collectible</h1>
                   </Col>
-                  <Col className="Multiple" lg={3} md={6}>
+                  <Col className="Multiple" lg={4} md={6}>
                     <p>Switch to Multiple</p>
                   </Col>
                 </Row>
@@ -36,7 +36,6 @@ const CreateItem = () => {
                 <h6><b>Upload file</b></h6>
                 <p>Drag or choose your file to upload </p>
                 <div class="form-Uploadfile">
-                  <input type="text" name="" id="" class="form-control" placeholder="" aria-describedby="helpId"></input>
                 </div>
               </div>
               <div className="left__ItemDetail">
@@ -57,6 +56,7 @@ const CreateItem = () => {
                       <div class="royalties">
                         <label for="">ROYALTIES</label>
                         <select class="form-control" name="" id="">
+                          <a name="" id="" class="btn btn-primary" href="#" role="button">123</a>
                           <option>10%</option>
                           <option>2</option>
                           <option>3</option>
@@ -78,9 +78,10 @@ const CreateItem = () => {
                   </Row>
                 </div>
               </div>
+              <div class="hr_left">
+              </div>
             </Col>
             <Col lg={5}>
-
               <div className="right__Preview">
                 <h1>Preview</h1>
                 <div className="img_preview">
@@ -88,15 +89,15 @@ const CreateItem = () => {
                 </div>
                 <div className="digital_price">
                   <Row>
-                    <Col className="digital" lg={5} md={6}>
+                    <Col className="digital" lg={8} md={6}>
                       <div className="digital_art">
                         <b>Amazing digital art</b>
                         <div className="img_avatar">
-                          <Image src={Avatar} />
+                          <Image src={Avatar} width="26px" height="26px" />
                         </div>
                       </div>
                     </Col>
-                    <Col className="price_ETH" lg={3} md={6}>
+                    <Col className="price_ETH" lg={4} md={6}>
                       <div className="price">
                         <b>1.125 ETH</b>
                         <p>3 in stock</p>
@@ -105,20 +106,23 @@ const CreateItem = () => {
                     </Col>
                   </Row>
                 </div>
+                <hr></hr>
+                <Row>
+                  <Col className="highest_bid" lg={8} md={6}>
+                    <div className="highest">
+                      <p>Highest bid <b>0.001 ETH</b></p>
+                    </div>
+                  </Col>
+                  <Col className="bid" lg={4} md={6}>
+                    <div className="newbid">
+                      <p>New bid</p>
+                    </div>
+                  </Col>
+                </Row>
+                <div class="clearAll">
+                  <p>Clear all</p>
+                </div>
               </div>
-              <hr></hr>
-              <Row>
-                <Col className="highest_bid" lg={5} md={6}>
-                  <div className="highest">
-                    <p>Highest bid <b>0.001 ETH</b></p>
-                  </div>
-                </Col>
-                <Col className="bid" lg={3} md={6}>
-                  <div className="digital_art">
-                    <p>New bid</p>
-                  </div>
-                </Col>
-              </Row>
             </Col>
           </Row>
         </div>
@@ -146,28 +150,39 @@ const CreateItem = () => {
                     </div>
                   </Col>
                   <Col className="Menu_right" lg={2} md={6}>
-                    <div class="button_a">a</div>
-                    <div class="button_a">a</div>
-                    <div class="button_a">a</div>
+                    <div class="button_right"><a name="" id="" class="btn btn-primary" href="#" role="button"></a>
+                    </div>
+                    <div class="button_right"><a name="" id="" class="btn btn-primary" href="#" role="button"></a>
+                    </div>
+                    <div class="button_right"><a name="" id="" class="btn btn-primary" href="#" role="button"></a>
+                    </div>
                   </Col>
                 </Row>
                 <div className="bottom_menu">
                   <Row>
                     <Col className="create_bottom" lg={3} md={6}>
                       <div className="circle_xanhbien"></div>
-                      <b>Create collection</b>
+                      <div class="name_color">
+                        <b>Create collection</b>
+                      </div>
                     </Col>
                     <Col className="create_bottom" lg={3} md={6}>
                       <div className="circle_xanhla"></div>
-                      <b>Crypto Legend - Professorion</b>
+                      <div class="name_color">
+                        <b>Crypto Legend - Professorion</b>
+                      </div>
                     </Col>
                     <Col className="create_bottom" lg={3} md={6}>
                       <div className="circle_hong"></div>
-                      <b>Crypto Legend - Professor</b>
+                      <div class="name_color">
+                        <b>Crypto Legend - Professor</b>
+                      </div>
                     </Col>
                     <Col lg={3} md={6} className="create_bottom">
                       <div className="circle_tim"></div>
-                      <b>Legend Photography</b>
+                      <div class="name_color">
+                        <b>Legend Photography</b>
+                      </div>
                     </Col>
                   </Row>
                 </div>
@@ -178,7 +193,8 @@ const CreateItem = () => {
           </Row>
         </div>
         <div className="button_view">
-          <a name="" id="" class="btn btn-primary" href="#" role="button">Create item</a>
+          <p>Create item</p>
+          <a name="" id="" class="btn btn-primary" href="#" role="button"><i class="fas fa-arrow-right"></i></a>
         </div>
       </Container>
     </createitem>
