47709d02d5fd2d1c62201532842eb72530ecda6b
hieu294 <tranthehieu12>
2021-12-01 19:35:15 +0700

    Update UploadItem

---
+++ src/components/ContentUploadItem/UploadItem.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import {
+  Navbar,
+  Container,
+  Nav,
+  Image,
+  Form,
+  Button,
+  FormControl,
+} from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+// import "../../assets/css/Uploaditem.css";
+
+import {Link} from "react-router-dom"
+
+function UploadItem() {
+  return (
+    <uploaditem id="uploaditem">
+    </uploaditem>
+  );
+}
+
+export default UploadItem;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Header from './Header_Footer/Header'
+import UploadItem from './ContentUploadItem/UploadItem'
 import Footer from './Header_Footer/Footer'
+
 
 function Home() {
   return (
     <div id="home">
       <Header />,
+      <UploadItem />,
       <Footer />
     </div>
   )
