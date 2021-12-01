1bf329cabfde8217dba74841ae2affa513f460a1
hieu294 <tranthehieu12>
2021-12-01 19:25:26 +0700

    update footer

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from './Header_Footer/Header'
+import Footer from './Header_Footer/Footer'
 
 function Home() {
   return (
     <div id="home">
-      <Header />
+      <Header />,
+      <Footer />
     </div>
   )
 }
