b40ac5f058718d6927d276ec77845c13518d80d0
huy <dhuy9916@gmail.com>
2021-12-02 12:02:05 +0700

    update screen max width 1920px

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import ScanWallet01 from "./components/ScanWallet01";
-
+import ScanWallet from "./components/ScanWallet";
+import ConnectWallet from "./components/ConnectWallet";
+import Home from "./components/Home";
+import {Routes,Route} from "react-router-dom"
 function App() {
   return (
     <div className="App">
-       <ScanWallet01/>
+      <Routes>
+        <Route path="/scanwallet01" element={<ScanWallet01 />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/scanwallet" element={<ScanWallet />} />
+        <Route path="/connectwallet" element={<ConnectWallet />} />
+      </Routes>
     </div>
   );
 }
--- src/components/ScanWallet01/content.js
+++ src/components/ScanWallet01/content.js
@@ -13,83 +13,96 @@ function Content() {
     <div>
       <connectwallect id="connectwallect01">
         {/* <Container> */}
-          <div class="main-connect-wallect">
-            <div class="button-header">
-              <div class="back-home">
-              <Nav.Link href="#backhome">
+        <div class="main-connect-wallect">
+          <div class="button-header">
+            <div class="back-home">
+              <Nav.Link href="/">
                 <i class="fas fa-arrow-left"></i> Back to home
-            </Nav.Link>
-              </div>
-              <div class="title-header">Connect Wallect</div>
+              </Nav.Link>
             </div>
-            <hr width="100%" align="center" />
+            <div class="title-header">Connect Wallect</div>
           </div>
-          <div class="main">
-            <div class="img-title-select">
-            <Image
-                  src={purple}
-                  class="img-picture"
-                  width="65px"
-                  height="65px"
-                />
-                <span>Coinbase Wallet</span>
-            </div>
-            <div class="img-title-select">
-            <Image
-                  src={blue}
-                  class="img-picture"
-                  width="65px"
-                  height="65px"
-                />
-                <span>Coinbase Wallet</span>
-            </div>
-            <div class="img-title-select">
-            <Image
-                  src={green}
-                  class="img-picture"
-                  width="65px"
-                  height="65px"
-                />
-                <span>Coinbase Wallet</span>
-            </div>
-            <div class="img-title-select">
-            <Image
-                  src={pink}
-                  class="img-picture"
-                  width="65px"
-                  height="65px"
-                />
-                <span>Coinbase Wallet</span>
-            </div>
-            <div class="title-main">
-               <span>Terms of service</span> 
-               <p>Please take a few minutes to read and understand Stacks Terms of Service. To continue, you’ll need to accept the terms of services by checking the boxes.</p>
-            </div>
-            <div class="img-main">
-            <Image
-                  src={main}
-                  class="img-picture"
-                  width="447px"
-                  height="160px"
-                />
-            </div>
-            <div class="item-check">
-              <input type="checkbox" class="check-box" defaultChecked={false} />
-              <span>I am at least 13 years old</span>
-            </div>
-            <div class="item-check">
-              <input type="checkbox" class="check-box" defaultChecked={false} />
-              <span>I agree Stack terms of service</span>
-            </div>
-            <div class="selection-button">
-            <Nav.Link href="#connectwallet">
-               <span> Cancel</span>
+          <hr width="100%" align="center" />
+        </div>
+        <div class="main">
+          <div class="img-title-select">
+            <Nav.Link href="scanwallet">
+              <Image
+                src={purple}
+                class="img-picture"
+                width="65px"
+                height="65px"
+              />
+              <span>Coinbase Wallet</span>
             </Nav.Link>
-            <Nav.Link href="#home">
-               <div class="choose">Get started now</div>
-            </Nav.Link>
-            </div>
           </div>
+
+          <div class="img-title-select">
+            <Nav.Link href="scanwallet">
+              <Image
+                src={blue}
+                class="img-picture"
+                width="65px"
+                height="65px"
+              />
+              <span>Coinbase Wallet</span>
+            </Nav.Link>
+          </div>
+          <div class="img-title-select">
+            <Nav.Link href="scanwallet">
+              <Image
+                src={green}
+                class="img-picture"
+                width="65px"
+                height="65px"
+              />
+              <span>Coinbase Wallet</span>
+            </Nav.Link>
+          </div>
+          <div class="img-title-select">
+            <Nav.Link href="scanwallet">
+              <Image
+                src={pink}
+                class="img-picture"
+                width="65px"
+                height="65px"
+              />
+              <span>Coinbase Wallet</span>
+            </Nav.Link>
+          </div>
+          <div class="title-main">
+            <span>Terms of service</span>
+            <p>
+              Please take a few minutes to read and understand Stacks Terms of
+              Service. To continue, you’ll need to accept the terms of services
+              by checking the boxes.
+            </p>
+          </div>
+          <div class="img-main">
+            <Image
+              src={main}
+              class="img-picture"
+              width="447px"
+              height="160px"
+            />
+          </div>
+          <div class="item-check">
+            <input type="checkbox" class="check-box" defaultChecked={false} />
+            <span>I am at least 13 years old</span>
+          </div>
+          <div class="item-check">
+            <input type="checkbox" class="check-box" defaultChecked={false} />
+            <span>I agree Stack terms of service</span>
+          </div>
+          <div class="selection-button">
+            <Nav.Link href="connectwallet">
+              <span> Cancel</span>
+            </Nav.Link>
+            <Nav.Link href="/">
+              <div class="choose">Get started now</div>
+            </Nav.Link>
+          </div>
+        </div>
         {/* </Container> */}
       </connectwallect>
     </div>
