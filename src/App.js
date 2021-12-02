57a3adeeffb45240583d16a1ab7344c3dbbcfabe
huy <dhuy9916@gmail.com>
2021-12-02 13:25:31 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import ScanWallet from "./components/ScanWallet";
-
+import ScanWallet01 from "./components/ScanWallet01";
+import Home from "./components/Home";
+import {Routes,Route} from "react-router-dom";
 function App() {
   return (
     <div className="App">
-       <ScanWallet/>
+      <Routes>
+       <Route path="/" element={<Home/>}/>
+       <Route path="/scanwallet" element={<ScanWallet/>}/>
+       <Route path="/scanwallet01" element={<ScanWallet01/>}/>
+     </Routes>
     </div>
   );
 }
--- src/components/ScanWallet/content.js
+++ src/components/ScanWallet/content.js
@@ -7,6 +7,7 @@ import blue from "../../assets/images/connect wallet/blue.png";
 import green from "../../assets/images/connect wallet/green.png";
 import pink from "../../assets/images/connect wallet/pink.png";
 import qrcode from "../../assets/images/scan wallet1_2/qrcode.png";
+
 function Content() {
   return (
     <div>
@@ -15,7 +16,7 @@ function Content() {
           <div class="main-connect-wallect">
             <div class="button-header">
               <div class="back-home">
-              <Nav.Link href="#backhome">
+              <Nav.Link href="/">
                 <i class="fas fa-arrow-left"></i> Back to home
             </Nav.Link>
               </div>
@@ -24,7 +25,7 @@ function Content() {
             <hr width="100%" align="center" />
           </div>
           <div class="main">
-            <div class="img-title-select">
+            <div onClick={clickImg1} class="img-title-select img-1">        
             <Image
                   src={purple}
                   class="img-picture"
@@ -32,8 +33,9 @@ function Content() {
                   height="65px"
                 />
                 <span>Coinbase Wallet</span>
+              
             </div>
-            <div class="img-title-select">
+            <div onClick={clickImg2} class="img-title-select img-2">
             <Image
                   src={blue}
                   class="img-picture"
@@ -42,7 +44,7 @@ function Content() {
                 />
                 <span>Coinbase Wallet</span>
             </div>
-            <div class="img-title-select">
+            <div onClick={clickImg3} class="img-title-select img-3">
             <Image
                   src={green}
                   class="img-picture"
@@ -51,7 +53,7 @@ function Content() {
                 />
                 <span>Coinbase Wallet</span>
             </div>
-            <div class="img-title-select">
+            <div onClick={clickImg4} class="img-title-select img-4">
             <Image
                   src={pink}
                   class="img-picture"
@@ -74,7 +76,7 @@ function Content() {
             </div>
            
             <div class="selection-button">
-            <Nav.Link href="#wallet01">
+            <Nav.Link href="scanwallet01">
                <span> Dont’s have a wallet app?</span>
             </Nav.Link>          
             </div>
@@ -84,5 +86,44 @@ function Content() {
     </div>
   );
 }
-
+function clickImg1(){
+  const img1 =  document.querySelector('.img-1');
+  let styleBoder = document.querySelector('.img-1').style.boxShadow;
+  if(styleBoder == "none" || styleBoder == ""){
+    img1.style.borderRadius = "10px";
+    img1.style.boxShadow = "0 0 5px gray";
+  }else{
+     img1.style.boxShadow = "none";  
+  }
+}
+function clickImg2(){
+  const img2 =  document.querySelector('.img-2');
+  let styleBoder = document.querySelector('.img-2').style.boxShadow;
+  if(styleBoder == "none" || styleBoder == ""){
+    img2.style.borderRadius = "10px";
+    img2.style.boxShadow = "0 0 5px gray";
+  }else{
+     img2.style.boxShadow = "none";  
+  }
+}
+function clickImg3(){
+  const img3 =  document.querySelector('.img-3');
+  let styleBoder = document.querySelector('.img-3').style.boxShadow;
+  if(styleBoder == "none" || styleBoder == ""){
+    img3.style.borderRadius = "10px";
+    img3.style.boxShadow = "0 0 5px gray";
+  }else{
+     img3.style.boxShadow = "none";  
+  } 
+}
+function clickImg4(){
+  const img4 =  document.querySelector('.img-4');
+  let styleBoder = document.querySelector('.img-4').style.boxShadow;
+  if(styleBoder == "none" || styleBoder == ""){
+    img4.style.borderRadius = "10px";
+    img4.style.boxShadow = "0 0 5px gray";
+  }else{
+     img4.style.boxShadow = "none";  
+  }
+}
 export default Content;
